0ff4bc672eb55c66e2015ae3c3536686ca927e19
Joffrmann29 <Joffrmann25@gmail.com>
2015-07-29 23:12:31 -0400

    Initial Commit

---
+++ JPMorganChaseSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JPMorganChaseSample
+//
+//  Created by Joffrey Mann on 7/29/15.
+//  Copyright (c) 2015 Nutech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
